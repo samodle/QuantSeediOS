e4b40c42685ef63947c8398cdaae4aca21603d03
SamOdle <sro27@cornell.edu>
2019-09-09 18:08:14 -0400

    first commit

---
+++ QuantSeed/QuantSeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuantSeed
+//
+//  Created by 599944 on 9/9/19.
+//  Copyright © 2019 QuantTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
